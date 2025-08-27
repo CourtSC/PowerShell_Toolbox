82b9021032fc10931ff9aa43e8cdb776a165e5eb
Scott Court <scott.court@adventhealth.com>
2025-08-27 11:51:34 -0400

    Add $Credential param to Install-RemotePrintDriver

--- Scripts/Remote_Print_Driver_Install.ps1
+++ Scripts/Remote_Print_Driver_Install.ps1
@@ -2,11 +2,13 @@ function Install-RemotePrintDriver {
     [CmdletBinding()]
     param (
         [Parameter(Position = 0)]
-        [System.Management.Automation.Runspaces.PSSession[]]$Session
+        [System.Management.Automation.Runspaces.PSSession[]]$Session,
+        [Parameter()]
+        [pscredential]$Credential
     )
 
-    if (-not $cred) {
-        $cred = Get-Credential -Message 'Enter your admin credentials:'
+    if (-not $Credential) {
+        $Credential = Get-Credential -Message 'Enter your admin credentials:'
     }
     if (-not $Session) {
         $computer = Read-Host -Prompt 'Computer Name'
@@ -14,21 +16,31 @@ function Install-RemotePrintDriver {
             Write-Host "Establishing PSSession with $computer." -ForegroundColor Green
             if (Test-Connection $computer -Count 2 -Quiet) {} else { throw } 
         } catch {
-            Write-Host "$computer is offline." -ForegroundColor Red
+            Write-Error "$computer is offline."
             return
         }
         try {
-            $Session = New-PSSession -ComputerName $computer -Credential $cred -ErrorAction Stop
+            $Session = New-PSSession -ComputerName $computer -Credential $Credential -ErrorAction Stop
         } catch { 
-            Write-Host "Unable to create session with $computer. Please confirm the computer name, verify it is online, and ensure you are entering your -a credentials when prompted." -ForegroundColor Red
+            Write-Error "Unable to create session with $computer. Please confirm the computer name, verify it is online, and ensure you are entering your -a credentials when prompted."
             return
         }
     }
-
-    $driverName = 'HP Universal Printing PCL 6 (v7.0.0)'
-    $driverPath = Get-PrinterDriver -Name $driverName | Select-Object -ExpandProperty InfPath | Split-Path
+    
+    # Sanity check for local driver installation
+    try {
+        $driverName = 'HP Universal Printing PCL 6 (v7.0.0)'
+        $driverPath = Get-PrinterDriver -Name $driverName | Select-Object -ExpandProperty InfPath | Split-Path
+        $driverInstalled = Test-Path $driverPath -ErrorAction Stop
+        if (-not $driverInstalled) { throw }
+    } catch {
+        Write-Error 'Driver path not found. Please ensure driver is installed on your computer before attempting to install driver on remote computer.'
+        return
+    }
+    
     $remoteDriverPath = $driverPath | Split-Path -Leaf
     $remotePath = 'C:\Temp\HPUPD'
+
 
     Invoke-Command -Session $Session -ScriptBlock {
         New-Item -Path 'C:\Temp\HPUPD' -ItemType Directory -Force | Out-Null
@@ -42,6 +54,7 @@ function Install-RemotePrintDriver {
     Invoke-Command -Session $Session -ScriptBlock {
         param($driverName, $remotePath, $remoteDriverPath)
         $ErrorActionPreference = 'Stop'
+        Write-Host "Installing driver on $env:COMPUTERNAME."
         Start-Service -Name Spooler
         pnputil.exe /add-driver ($remotePath + '\' + $remoteDriverPath + '\hpcu250u.inf') /install
         Add-PrinterDriver -Name $driverName
