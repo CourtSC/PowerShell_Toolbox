11a3780253115f901ce4a21374793bb54fb923a3
Scott Court <scott.court@adventhealth.com>
2026-03-19 08:49:05 -0400

    .

--- Scripts/pcl_xl_error_fix.ps1
+++ Scripts/pcl_xl_error_fix.ps1
@@ -18,7 +18,7 @@ $sb = {
 
     Get-ChildItem -Path 'C:\Windows\System32\spool\drivers' -Recurse -Filter '*.gpd' | ForEach-Object { Remove-Item $_.FullName }
     Get-ChildItem -Path 'C:\Windows\System32\spool\PRINTERS' | ForEach-Object { Remove-Item $_.FullName }
-    
+
     Restart-Service Spooler -ErrorAction SilentlyContinue
     Restart-Computer -Force
 }
